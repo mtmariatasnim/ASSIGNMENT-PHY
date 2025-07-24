1d4c91182dc9a1caa28ec4c8c3777e231d344cc6
Maria tasnim <mariatasnimmt@gmail.com>
2025-07-24 17:56:09 +0100

    Update PROBLEM -2.c

--- PROBLEM -2.c
+++ PROBLEM -2.c
@@ -3,8 +3,8 @@
 
 int main() {
     double theta = 45.0;
-    double theta_radous = theta * (3.1416 / 180);
-    double ratio = 1 / sin(theta_radous);
+    double theta_radius = theta * (3.1416 / 180);
+    double ratio = 1 / sin(theta_radius);
     printf("The ratio a/lambda is: %.2f\n", ratio);
 
     return 0;
